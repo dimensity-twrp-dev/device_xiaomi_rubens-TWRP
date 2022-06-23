947a038ba4bfc4416ea87c69c58e205614095aba
Woomymy <github@woomy.ovh>
2022-06-23 18:45:31 +0200

    mtk_plpath_utils: Cleanup code

--- bootctrl/BootControl.cpp
+++ bootctrl/BootControl.cpp
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <cstdint>
 #define LOG_TAG "android.hardware.boot@1.2-mtkimpl"
 
 #include <memory>
