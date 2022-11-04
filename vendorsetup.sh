bf7ea3023f01ddba9ca16bc1fe2f9b3ea415402a
hank9999 <hank9999@qq.com>
2022-11-04 19:52:10 +0800

    fix orangefox support

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -22,26 +22,27 @@ FOX_MANIFEST_ROOT=$(gettop)
 if [ -f $FOX_MANIFEST_ROOT/bootable/recovery/orangefox_defaults.go -a -f $FOX_MANIFEST_ROOT/bootable/recovery/orangefox.mk ]; then
     if [ -z "$TW_DEFAULT_LANGUAGE" ]; then
         unset TW_DEFAULT_LANGUAGE
-        export TW_DEFAULT_LANGUAGE="zh_CH"
+        export TW_DEFAULT_LANGUAGE="zh_CN"
     fi
+
+    # Build
     export LC_ALL="C"
     export FOX_VERSION=$(date +%y.%m.%d)
-    export OF_AB_DEVICE=1
-    export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-    export OF_NO_MIUI_PATCH_WARNING=1
-    export OF_FBE_METADATA_MOUNT_IGNORE=1
-    export OF_PATCH_AVB20=1
-    export OF_PATCH_VBMETA_FLAG=1
-    export OF_USE_GREEN_LED=0
-    export OF_FLASHLIGHT_ENABLE=0
-    export OF_SCREEN_H=3200
-    export OF_STATUS_H=50
-    export OF_STATUS_INDENT_LEFT=85
-    export OF_STATUS_INDENT_RIGHT=85
-    export OF_HIDE_NOTCH=1
-    export OF_CLOCK_POS=1
-    export OF_ALLOW_DISABLE_NAVBAR=0
+    export ALLOW_MISSING_DEPENDENCIES=true
     export TARGET_DEVICE_ALT="rubens"
+    export OF_VIRTUAL_AB_DEVICE=1
+    export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+    export OF_MAINTAINER="D8100-9000-TWRP-Device-Tree Team"
+
+    # Magiskboot
+    export OF_USE_MAGISKBOOT=1
+    export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+    export OF_PATCH_VBMETA_FLAG=1
+
+    # Magisk
+    export FOX_USE_SPECIFIC_MAGISK_ZIP="$FOX_MANIFEST_ROOT/device/xiaomi/rubens/Magisk/Magisk-v25.2.zip"
+
+    # Binaries
     export FOX_USE_BASH_SHELL=1
     export FOX_ASH_IS_BASH=1
     export FOX_USE_NANO_EDITOR=1
@@ -53,15 +54,35 @@ if [ -f $FOX_MANIFEST_ROOT/bootable/recovery/orangefox_defaults.go -a -f $FOX_MA
     export FOX_USE_XZ_UTILS=1
     export FOX_REPLACE_BUSYBOX_PS=1
     export OF_ENABLE_LPTOOLS=1
-    export OF_USE_NEW_MAGISKBOOT=1
+
+    # Decryption
+    export OF_FBE_METADATA_MOUNT_IGNORE=1
     export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
     export OF_NO_RELOAD_AFTER_DECRYPTION=1
-    export MAGISK_VER=25.1
-    export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-    export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-    export FOX_R11=1
-    export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+
+    # Screen
+    export OF_SCREEN_H=2400
+    export OF_STATUS_H=50
+    export OF_STATUS_INDENT_LEFT=85
+    export OF_STATUS_INDENT_RIGHT=85
+    export OF_HIDE_NOTCH=1
+    export OF_CLOCK_POS=1
+    export OF_ALLOW_DISABLE_NAVBAR=0
+
+    # Backup
     export OF_QUICK_BACKUP_LIST="/boot;/vendor_boot;/data;"
+
+    # OTA & MIUI
+    export OF_NO_MIUI_PATCH_WARNING=1
+    export OF_PATCH_AVB20=1
+    # export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+    export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+    export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+
+    # Others
+    export OF_USE_GREEN_LED=0
+    export OF_FLASHLIGHT_ENABLE=0
+    
 
     if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
         export | grep "FOX" >>$FOX_BUILD_LOG_FILE
